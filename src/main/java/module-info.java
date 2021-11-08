b2a3e08a8e19e49af1e4a889b6d7396210f93f23
AngelRabasco <rabascoangel01@gmail.com>
2021-11-08 10:22:04 +0100

    Created database connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.AngelRabasco.OnlyTracks {
 	requires transitive javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 
 	opens org.AngelRabasco.OnlyTracks to javafx.fxml;
 
